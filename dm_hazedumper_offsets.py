281cf14d49af488be5581c6f916b1d3d82f4a527
MayureshNehe <mayuresh@iagentpro.com>
2024-12-26 12:03:25 +0530

    Add files via upload

---
+++ dm_hazedumper_offsets.py
@@ -0,0 +1,17 @@
+dwEntityList = 27346664
+dwLocalPlayerController = 27676288
+dwLocalPlayerPawn = 25595080
+dwViewAngles = 27825936
+
+
+m_bIsScoped = 9192
+m_iHealth = 836
+m_iFOVStart = 532
+m_vOldOrigin = 4900
+m_vecVelocity = 1024
+m_vecViewOffset = 3248
+m_hActiveWeapon = 88
+m_iItemDefinitionIndex = 442
+m_iClip1 = 5752
+m_iObserverMode = 64
+m_hObserverTarget = 68
